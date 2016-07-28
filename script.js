d84cd1bdf4b7afcb708844575aa5488441430975
Tudor Lehene <tlehene@veinteractive.com>
2016-07-28 15:48:14 +0300

    First files added to our project

---
+++ script.js
@@ -0,0 +1,22 @@
+var Pokemon = (function(){
+	var choseHomePlayer = function() {
+		var choice = $('.choice');
+		choice.on('click', function() {
+			var homePokeIsChosen = $('#home').has('.choice').length > 0;
+			if(homePokeIsChosen) {
+				var awayPokeIsChosen = $('#away').has('.choice').length === 1;
+				if(awayPokeIsChosen) {
+					$.growlUI('Growl Notification', 'Have a nice day!'); 
+					return;
+				}
+				$(this).prependTo('#away').animate({top: 900, left: 200},300);
+				return;
+			}
+			$(this).prependTo('#home');
+		});
+	}
+	
+	return {
+		choseHomePlayer: choseHomePlayer
+	}
+})();
